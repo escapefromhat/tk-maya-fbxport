11c3047494eed9e38651f6a1892b7cfedce34597
Jason Angell <jangell@prodicle.com>
2020-07-06 14:49:15 -0400

    adding in missing files

---
+++ python/app/xporter.py
@@ -0,0 +1,21 @@
+# Jason Angell
+# Parallux
+# Execution logic for tk-maya-fbxport
+
+import sgtk
+import os
+import time
+
+# standard toolkit logger
+logger = sgtk.platform.get_logger(__name__)
+SDFramework = sgtk.platform.get_framework('tk-framework-scene-description')
+MTW = SDFramework.import_module('utils.MayaTransformWriter')
+
+def xport(app_instance):
+	# grab the maya exporter from the scene description framework
+	mtw = MTW.MayaTransformWriter('C:\\fake\\ass\\path')
+	mtw.publish_collapsed(app_instance)
+
+
+def center_pivots(app_instance):
+	MTW.center_pivots()
